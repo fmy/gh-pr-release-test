3ae23d85c59473709b2eda566ccb8de3f556f6a7
Fumiya Karasawa <krswfmy@gmail.com>
2019-08-08 09:51:00 +0900

    api token (#4)

--- pr-release.js
+++ pr-release.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 const release = require('github-pr-release')
 
 const config = {
-  token: '',
+  token: process.env.GITHUB_API_TOKEN,
   owner: 'fmy',
   repo:  'gh-pr-release-test',
   head:  'master',

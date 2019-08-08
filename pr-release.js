4ed7be69d9291b68c06deba68f70030b7904c66a
Fumiya Karasawa <krswfmy@gmail.com>
2019-08-08 10:22:23 +0900

    head from argv

--- pr-release.js
+++ pr-release.js
@@ -2,11 +2,16 @@ require('dotenv').config()
 
 const release = require('github-pr-release')
 
+const head = process.argv[2]
+if (!head) {
+  process.exit(1)
+}
+
 const config = {
   token: process.env.GITHUB_API_TOKEN,
   owner: 'fmy',
   repo:  'gh-pr-release-test',
-  head:  'master',
+  head,
   base:  'release',
 }
 
